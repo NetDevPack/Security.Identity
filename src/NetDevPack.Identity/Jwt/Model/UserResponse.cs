b0efe17bba46d3d92bfd669d07683985c0972954
Paulo Monteiro <pmsj.developer@gmail.com>
2020-06-11 17:06:20 -0300

    implemented generic Key to IdentityUser

--- src/NetDevPack.Identity/Jwt/Model/UserResponse.cs
+++ src/NetDevPack.Identity/Jwt/Model/UserResponse.cs
@@ -1,9 +1,9 @@
 ﻿namespace NetDevPack.Identity.Jwt.Model
 {
-    public class UserResponse
+    public class UserResponse<TKey>
     {
         public string AccessToken { get; set; }
         public double ExpiresIn { get; set; }
-        public UserToken UserToken { get; set; }
+        public UserToken<TKey> UserToken { get; set; }
     }
 }
--- src/NetDevPack.Identity/Jwt/Model/UserToken.cs
+++ src/NetDevPack.Identity/Jwt/Model/UserToken.cs
@@ -2,9 +2,9 @@
 
 namespace NetDevPack.Identity.Jwt.Model
 {
-    public class UserToken
+    public class UserToken<T>
     {
-        public string Id { get; set; }
+        public T Id { get; set; }
         public string Email { get; set; }
         public IEnumerable<UserClaim> Claims { get; set; }
     }
